3dc2a507910fb481da1a8901bd28e57fa876084e
Noel B.A <nbalonso@gmail.com>
2013-12-17 19:03:31 +0300

    added friendly view on widget tables
     when empty values exist. Issue #57

--- app/views/widgets/filevault_widget.php
+++ app/views/widgets/filevault_widget.php
@@ -4,25 +4,35 @@
 
 				<div class="panel-heading">
 
-					<h3 class="panel-title"><i class="icon-lock"></i> Encyption status</h3>
+					<h3 class="panel-title"><i class="icon-lock"></i> Filevault 2 status</h3>
 				
 				</div>
 
 				<div class="list-group">
 
 				<?	$fv = new filevault_status_model(); 
-$sql= "SELECT count(1) AS count,filevault_status FROM  filevault_status INNER join machine on filevault_status.serial_number = machine.serial_number  where machine_desc like \"MacBook%\"";
-#$sql = "SELECT count(1) AS count, filevault_status FROM filevault_status GROUP BY filevault_status ORDER BY filevault_status";
+					$sql = "SELECT count(1) AS count,
+							filevault_status FROM filevault_status
+							GROUP BY filevault_status
+							ORDER BY filevault_status";
 					$cnt = 0;
 				?>
 					<?foreach($fv->query($sql) as $obj):?>
-					<?//$status = array_key_exists($obj->status, $class_list) ? $class_list[$obj->status] : 'danger'?>
-					<a href="<?=url('show/listing/security#'.$obj->filevault_status)?>" class="list-group-item list-group-item-<?//=$status?>">
-						<span class="badge"><?=$obj->count?></span>
-						<?=$obj->filevault_status?>
-					<?$cnt++?>
-					</a>
+							<?if (empty($obj->filevault_status)):?>
+								<a class="list-group-item"><?=lang('unknown')?>
+									<span class="badge pull-right"><?=$obj->count?></span>
+								</a>
+							<?else:?>
+								<a href="<?=url('show/listing/security#'.$obj->filevault_status)?>" class="list-group-item">
+									<span class="badge"><?=$obj->count?></span>
+									<?=$obj->filevault_status?>
+								</a>
+							<?endif?>
+							<?$cnt++?>
 					<?endforeach?>
+					<?if( ! $cnt):?>
+						<span class="list-group-item">No Filevault status available</span>
+						<?endif?>
 
 				</div>
 
